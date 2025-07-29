3252468ba9f58dd0f5599d5bace39a53b005a20a
Zain-Ali-712 <za9690000@gmail.com>
2025-07-29 15:37:34 +0500

    Day 2 of starting coding this project

--- wristro/src/app/layout.tsx
+++ wristro/src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className="bg-radial-gradient text-gray-900">
+      <body className="bg-radial-gradient text-gray-950">
         <Navbar />
         <main className="min-h-screen">{children}</main>
       </body>
